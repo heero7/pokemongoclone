dc66b3424b15166717a5fdb3309c351191a32868
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2017-12-14 21:49:35 -0600

    Application now starts zoomed in on users current location

--- pokemongo/controller/MapViewController.swift
+++ pokemongo/controller/MapViewController.swift
@@ -13,6 +13,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    var updateCount = 0
     var manager = CLLocationManager()
     
     override func viewDidLoad() {
@@ -22,10 +23,31 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
             // realize that the authorization is only in use
             mapView.showsUserLocation = true
+            // ask for current location, continually update
+            manager.startUpdatingLocation()
         } else {
             manager.requestWhenInUseAuthorization()
         }
     }
 
-
+    /*
+     We'll make sure the app starts zoomed in to the current location of the phone
+     */
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // play with this count to get the right location
+        
+        if updateCount < 3 {
+            
+            if let coordinate = manager.location?.coordinate {
+                let region = MKCoordinateRegionMakeWithDistance(coordinate, 1000, 1000)
+                mapView.setRegion(region, animated: false)
+            }
+            
+            updateCount += 1
+        }
+        
+        
+        
+    }
 }
