a3275e25f1a427039ab36c28a1f506945e94b0c5
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2017-12-15 23:28:35 -0600

    Added map image for button to get back to mapview, added pokedex view controller

---
+++ pokemongo/controller/PokedexViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PokedexViewController.swift
+//  pokemongo
+//
+//  Created by Kevin Mudiandambo on 12/15/17.
+//  Copyright © 2017 Kevin Mudiandambo. All rights reserved.
+//
+
+import UIKit
+
+class PokedexViewController: UIViewController {
+    
+    @IBOutlet weak var pokedexTableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func returnToMapView(_ sender: Any) {
+    }
+    
+}
